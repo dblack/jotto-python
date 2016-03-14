dc7cfa902ee882d41d33d9e7c0558f2499d48d42
David A. Black <dblack@rubypal.com>
2016-03-14 15:39:58 -0400

    Moving to class-based structure

--- jotto/jotto.py
+++ jotto/jotto.py
@@ -2,11 +2,12 @@ class WordLengthException(Exception):
 	def __init__(self):
 		Exception.__init__(self, "Jotto words have to have exactly five letters")
 
-def load_dictionary(filename="fives"):
-	return [w.strip() for w in open(filename).readlines()]
+class Game():
+	def load_dictionary(self, filename="fives"):
+		self.dictionary = [w.strip() for w in open(filename).readlines()]
 
-def matching_letter_count(word1, word2):
-	if len(word1) != 5 or len(word2) != 5:
-		raise WordLengthException()
+	def matching_letter_count(self, word1, word2):
+		if len(word1) != 5 or len(word2) != 5:
+			raise WordLengthException()
 
-	return len([n for n in range(0,5) if word1[n] == word2[n]])
+		return len([n for n in range(0,5) if word1[n] == word2[n]])
--- test/test_jotto.py
+++ test/test_jotto.py
@@ -1,29 +1,33 @@
 import unittest
 import jotto
-from jotto.jotto import matching_letter_count, load_dictionary
+from jotto.jotto import Game
 
 class JottoTestCase(unittest.TestCase):
+	def setUp(self):
+		self.game = Game()
+		self.game.load_dictionary("jotto/fives")
+
 	def test_matching_letter_count(self):
 		"""How many matches are there between words?"""
 
-		self.assertEqual(matching_letter_count('house', 'haunt'), 2)
+		self.assertEqual(self.game.matching_letter_count('house', 'haunt'), 2)
 
 	def test_zero_letters_match(self):
 		"""No letters match positionally"""
 
-		self.assertEqual(matching_letter_count('house', 'strip'), 0)
+		self.assertEqual(self.game.matching_letter_count('house', 'strip'), 0)
 
 	def test_word_length_exception_for_short_words(self):
 		"""Raise exception if word too short"""
 
-		self.assertRaises(jotto.jotto.WordLengthException, jotto.jotto.matching_letter_count, 'house', 'huh')
+		self.assertRaises(jotto.jotto.WordLengthException, self.game.matching_letter_count, 'house', 'huh')
 
 	def test_word_length_exception_for_long_words(self):
 		"""Raise exception if word too long"""
 
-		self.assertRaises(jotto.jotto.WordLengthException, jotto.jotto.matching_letter_count, 'house', 'building')
+		self.assertRaises(jotto.jotto.WordLengthException, self.game.matching_letter_count, 'house', 'building')
 
 	def test_dictionary_words_are_all_five_letters(self):
-		dict = load_dictionary("jotto/fives")
+		dict = self.game.dictionary
 		over_five = [word for word in dict if len(word.strip()) > 5]
 		self.assertEqual(len(over_five), 0)
