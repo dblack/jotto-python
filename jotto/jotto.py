105aca9a2bc3dcbfb938074cf0b3f667ea8342c1
David A. Black <dblack@rubypal.com>
2016-03-14 12:19:55 -0400

    No need to make the two words into letter-lists

--- jotto/jotto.py
+++ jotto/jotto.py
@@ -4,10 +4,7 @@ class WordLengthException(Exception):
 
 
 def matching_letter_count(word1, word2):
-	letters1 = list(word1)
-	letters2 = list(word2)
-
-	if len(letters1) != 5 or len(letters2) != 5:
+	if len(word1) != 5 or len(word2) != 5:
 		raise WordLengthException()
 
-	return len([n for n in range(0,5) if letters1[n] == letters2[n]])
+	return len([n for n in range(0,5) if word1[n] == word2[n]])
