a5a715b5d3fdd7a3b0244de19a0dfe1cbaf1306c
David A. Black <dblack@rubypal.com>
2016-03-14 11:33:33 -0400

    Remove temporary variables in tests

--- test/test_jotto.py
+++ test/test_jotto.py
@@ -5,17 +5,13 @@ from jotto.jotto import matching_letter_count
 class JottoTestCase(unittest.TestCase):
 	def test_matching_letter_count(self):
 		"""How many matches are there between words?"""
-		word1 = 'house'
-		word2 = 'haunt'
 
-		self.assertEqual(matching_letter_count(word1, word2), 2)
+		self.assertEqual(matching_letter_count('house', 'haunt'), 2)
 
 	def test_zero_letters_match(self):
 		"""No letters match positionally"""
-		word1 = 'house'
-		word2 = 'strip'
 
-		self.assertEqual(matching_letter_count(word1, word2), 0)
+		self.assertEqual(matching_letter_count('house', 'strip'), 0)
 
 	def test_word_length_exception_for_short_words(self):
 		"""Raise exception if word too short"""
