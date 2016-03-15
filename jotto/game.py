f1376c5adef91f1814d7acd781c0c86fac698248
David A. Black <dblack@rubypal.com>
2016-03-15 08:53:39 -0400

    Game-playing logic

--- jotto/game.py
+++ jotto/game.py
@@ -1,4 +1,4 @@
-import player
+import player, utils
 
 class Game():
 	def __init__(self):
@@ -8,8 +8,20 @@ class Game():
 	def get_guess_from_player(self):
 		self.player_guess = input("Your guess: ")
 
+
 	def play(self):
+		self.computer.word = self.computer.choose_a_word()
 		count = 0
-		while count != 5:
+		self.player_guess = ""
+		player_wins = False
+
+		while (count != 5) and (player_wins == False):
+			self.get_guess_from_player()
+			print(utils.matching_letter_count(self.player_guess, self.computer.word))
+			player_wins = self.player_guess == self.computer.word
 			count = self.computer.guess_players_word()
-		print("I win")
+
+		if player_wins:
+			print("You win!")
+		else:
+			print("I win! My word was " + self.computer.word)
--- jotto/player.py
+++ jotto/player.py
@@ -1,20 +1,20 @@
 import utils, dictionary
 
-class Player():
+class Player(dictionary.DictionaryOwner):
 	pass
 
-class Computer(dictionary.DictionaryOwner, Player):
-	def choose_my_word(self):
-		self.my_word = self.dictionary.random_word()
+class Computer(Player):
+	def choose_a_word(self):
+		return self.dictionary.random_word()
 
 	def winnow_dictionary(self, word, count):
 		self.dictionary.words = [w for w in self.dictionary.words if utils.matching_letter_count(word, w) == count]
 
 	def guess_players_word(self):
-		self.choose_my_word()
-		count = int(input(self.my_word + " Count: "))
-		self.winnow_dictionary(self.my_word, count)
+		guess = self.choose_a_word()
+		count = int(input(guess + ": "))
+		self.winnow_dictionary(guess, count)
 		return count
 
-class Human(dictionary.DictionaryOwner, Player):
+class Human(Player):
 	pass
