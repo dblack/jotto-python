7b31da973e43c292f1921056895e2f2085fc267d
David A. Black <dblack@rubypal.com>
2016-03-15 09:24:20 -0400

    Move a function to the Computer class

--- jotto/game.py
+++ jotto/game.py
@@ -5,10 +5,6 @@ class Game():
 		self.computer = player.Computer()
 		self.human = player.Human()
 
-	def get_guess_from_player(self):
-		self.player_guess = input("Your guess: ")
-
-
 	def play(self):
 		self.computer.word = self.computer.choose_a_word()
 		count = 0
@@ -16,9 +12,9 @@ class Game():
 		player_wins = False
 
 		while (count != 5) and (player_wins == False):
-			self.get_guess_from_player()
-			print(utils.matching_letter_count(self.player_guess, self.computer.word))
-			player_wins = self.player_guess == self.computer.word
+			self.computer.get_guess_from_player()
+			print(utils.matching_letter_count(self.computer.player_guess, self.computer.word))
+			player_wins = self.computer.player_guess == self.computer.word
 			count = self.computer.guess_players_word()
 
 		if player_wins:
--- jotto/player.py
+++ jotto/player.py
@@ -10,6 +10,9 @@ class Computer(Player):
 	def winnow_dictionary(self, word, count):
 		self.dictionary.words = [w for w in self.dictionary.words if utils.matching_letter_count(word, w) == count]
 
+	def get_guess_from_player(self):
+		self.player_guess = input("Your guess: ")
+
 	def guess_players_word(self):
 		guess = self.choose_a_word()
 		count = int(input(guess + ": "))
