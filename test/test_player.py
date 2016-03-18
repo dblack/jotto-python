b0c71fb1aa3697807a97b8e99529285ecccb4c7d
David A. Black <dblack@rubypal.com>
2016-03-18 16:46:47 -0400

    Experimental testing of keyboard IO

--- test/test_player.py
+++ test/test_player.py
@@ -15,4 +15,18 @@ class HumanTestCase(unittest.TestCase):
         self.assertEqual(self.human.valid_guess(), False)
 
         self.human.current_guess = "right"
-        self.assertEqual(self.human.valid_guess(), True)       
+        self.assertEqual(self.human.valid_guess(), True)
+
+    def test_guess_computers_word(self):
+        input_string = StringIO.StringIO("tiny\nright")
+        sys.stdin = input_string
+        output_string = StringIO.StringIO()
+        sys.stdout = output_string
+
+        self.human.guess_computers_word()
+
+        sys.stdin = sys.__stdin__
+        sys.stdout = sys.__stdout__
+
+        output_string.seek(0)
+        self.assertEqual(output_string.readline(), "Your guess: Five-letter words from the dictionary only\n")
