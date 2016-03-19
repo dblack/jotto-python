c69ae00372dcd4dfac352cc9a5eb69025a46abb9
David A. Black <dblack@rubypal.com>
2016-03-19 05:48:52 -0400

    Remove docstrings in test file. (Probably should add more instead....)

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,23 +3,15 @@ from jotto import utils
 
 class UtilsTestCase(unittest.TestCase):
     def test_matching_letter_count(self):
-        """How many matches are there between words?"""
-
         self.assertEqual(utils.matching_letter_count('house', 'haunt'), 2)
 
     def test_zero_letters_match(self):
-        """No letters match positionally"""
-
         self.assertEqual(utils.matching_letter_count('house', 'strip'), 0)
 
     def test_word_length_exception_for_short_words(self):
-        """Raise exception if word too short"""
-
         self.assertRaises(utils.WordLengthException, utils.matching_letter_count, 'house', 'huh')
 
     def test_word_length_exception_for_long_words(self):
-        """Raise exception if word too long"""
-
         self.assertRaises(utils.WordLengthException, utils.matching_letter_count, 'house', 'building')
 
 
