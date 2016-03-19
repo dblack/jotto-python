15f717b3299adc99e2da1ddb685d1caf46b40a13
David A. Black <dblack@rubypal.com>
2016-03-18 20:22:48 -0400

    Wild and wacky rewrite of test. Educational, anyway :-)

--- test/test_player.py
+++ test/test_player.py
@@ -1,32 +1,37 @@
 import unittest
 import sys
-import StringIO
+from StringIO import StringIO
 from jotto import player
+from contextlib import contextmanager
 
 class HumanTestCase(unittest.TestCase):
     def setUp(self):
         self.human = player.Human()
 
-    def test_valid_guess(self):
-        self.human.current_guess = "tiny"
-        self.assertEqual(self.human.valid_guess(), False)
+    def test_guess_to_short_a_word(self):
+        self.guess_a_word("tiny")
 
-        self.human.current_guess = "lengthy"
-        self.assertEqual(self.human.valid_guess(), False)
+    def test_guess_too_long_a_word(self):
+        self.guess_a_word("lengthy")
 
-        self.human.current_guess = "right"
-        self.assertEqual(self.human.valid_guess(), True)
+    def test_guess_a_nonexistent_word(self):
+        self.guess_a_word("blaha")
 
-    def test_guess_computers_word(self):
-        input_string = StringIO.StringIO("tiny\nright")
-        sys.stdin = input_string
-        output_string = StringIO.StringIO()
-        sys.stdout = output_string
+    def guess_a_word(self, word):
+        with self.switch_io(word + "\nright") as output_stream:
+            self.human.guess_computers_word()
+ 
+        self.assertEqual(output_stream.read(), "Your guess: Five-letter words from the dictionary only\nYour guess: ")
+        self.assertEqual(self.human.current_guess, "right")
 
-        self.human.guess_computers_word()
+    @contextmanager
+    def switch_io(self, input_string):
+        sys.stdin = StringIO(input_string)
+        output_stream = StringIO()
+        sys.stdout = output_stream
 
+        yield output_stream
+
+        output_stream.seek(0)
         sys.stdin = sys.__stdin__
         sys.stdout = sys.__stdout__
-
-        output_string.seek(0)
-        self.assertEqual(output_string.readline(), "Your guess: Five-letter words from the dictionary only\n")
