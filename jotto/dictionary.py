ef222aa7e467d221e0d0a83184cb27cedf5b35c4
David A. Black <dblack@rubypal.com>
2016-03-19 23:08:31 -0400

    Only save filter if count > 0

--- jotto/dictionary.py
+++ jotto/dictionary.py
@@ -33,7 +33,8 @@ class Dictionary():
                 return word
 
     def winnow(self, guess, count):
-        self.filters.append((guess, count))
+        if count > 0:
+            self.filters.append((guess, count))
 
 class DictionaryOwner(object):
     def __init__(self):
